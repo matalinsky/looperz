26727d92d4b9122b7633897cdc3755c4d2b5fdb0
matalinsky <153123608+matalinsky@users.noreply.github.com>
2026-04-22 08:49:06 -0300

    Update driver.js

--- driver.js
+++ driver.js
@@ -26,7 +26,7 @@ function getSpeedMult(zone = curTown) {
     else if (zone === 6) speedMult *= getRitualBonus(150, 200, 1.4);
     else if (zone === 7) speedMult *= getRitualBonus(200, 250, 1.2);
     else if (zone === 8) speedMult *= getRitualBonus(250, 300, 1);
-    speedMult *= getRitualBonus(300, 666, .181967);
+    speedMult *= getRitualBonus(300, 666, .274);
     
     // Chronomancy
     speedMult *= (getSkillBonus("Chronomancy")*1.5);
